83304bad53fb1607e24012745a2a37af52660140
Hardly Difficult <hardlydiff@gmail.com>
2019-09-22 15:16:23 -0700

    Samples (#90)
    * Base samples dir
    
    * Bump version to 0.4.2
    
    * Finish initial sample tests
    
    * Lint

---
+++ samples/truffle/test/tokens/dai.js
@@ -0,0 +1,16 @@
+const { tokens } = require("hardlydifficult-ethereum-contracts");
+
+contract("tokens / DAI", accounts => {
+  const tokenOwner = accounts[0];
+  let token;
+
+  before(async () => {
+    token = await tokens.dai.deploy(web3, tokenOwner);
+  });
+
+  it("Can mint from owner account", async () => {
+    await token.mint(accounts[1], 100, { from: tokenOwner });
+    const balance = await token.balanceOf(accounts[1]);
+    assert.equal(balance.toString(), 100);
+  });
+});
---
+++ samples/truffle/test/tokens/usdc.js
@@ -0,0 +1,17 @@
+const { tokens } = require("hardlydifficult-ethereum-contracts");
+
+contract("tokens / USDC", accounts => {
+  const proxyOwner = accounts[9]; // must be different from the tokenOwner
+  const tokenOwner = accounts[0];
+  let token;
+
+  before(async () => {
+    token = await tokens.usdc.deploy(web3, proxyOwner, tokenOwner);
+  });
+
+  it("Can mint from owner account", async () => {
+    await token.mint(accounts[1], 100, { from: tokenOwner });
+    const balance = await token.balanceOf(accounts[1]);
+    assert.equal(balance.toString(), 100);
+  });
+});
