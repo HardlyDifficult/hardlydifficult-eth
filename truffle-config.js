e510846006dd5c8f0917e37170783b1c41848ee0
Hardly Difficult <hardlydiff@gmail.com>
2019-10-10 15:55:46 -0700

    Add eth-gas-reporter (#136)

--- truffle-config.js
+++ truffle-config.js
@@ -74,29 +74,28 @@ module.exports = {
     // }
   },
 
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-    reporter: "eth-gas-reporter",
-    useColors: true,
-    reporterOptions: {
-      currency: "USD",
-      gasPrice: 5
-    }
-  },
-
   // Configure your compilers
   compilers: {
     solc: {
       // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: true,
+          runs: 2000000
+        }
+        //  evmVersion: "byzantium"
+      }
+    }
+  },
+  mocha: {
+    reporter: "eth-gas-reporter",
+    useColors: true,
+    reporterOptions: {
+      currency: "USD",
+      excludeContracts: ["Migrations"],
+      gasPrice: 5
     }
   }
 };
