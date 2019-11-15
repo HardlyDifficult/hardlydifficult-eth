7adce19060c3f8110b583965e55ca33241b63b8c
Hardly Difficult <hardlydiff@gmail.com>
2019-11-14 17:26:38 -0800

    BigDiv coverage to 100% (#226)
    * BigDiv coverage to 100%
    
    * Remove .only
    
    * Update bigDivNumbersArray.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
+const BigNumber = require("bignumber.js");
+
 module.exports = {
-  MAX_GAS: 6700000
+  MAX_GAS: 6700000,
+  MAX_UINT: new BigNumber(2)
+    .pow(256)
+    .minus(1)
+    .toFixed()
 };
