e61ef79ed04beebc5debd55c4cf19cc5057c6680
Hardly Difficult <hardlydiff@gmail.com>
2019-12-11 15:15:41 -0800

    Update .solcover.js (#260)

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,8 @@ module.exports = {
   skipFiles: ['Migrations.sol'], // , 'test-artifacts/', 'test-helpers/'
   providerOptions: {
     total_accounts: 100,
-    default_balance_ether: 10000000000
+    default_balance_ether: 10000000000,
+    hardfork: 'istanbul'
   },
   mocha: {
     reporter: null
