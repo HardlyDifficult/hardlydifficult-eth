3678e928f8cde10c79215655e06d6a051722c225
NickCuso <hardlydiff@gmail.com>
2019-12-14 17:32:02 -0800

    +helpers

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   constants: require("./src/constants"),
+  helpers: require("./src/helpers"),
   tokens: require("./src/tokens"),
   protocols: require("./src/protocols"),
   utils: require("./src/utils")
