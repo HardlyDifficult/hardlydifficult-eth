9230d769d223bccd78bae6f429948f518e508664
Hardly Difficult <hardlydiff@gmail.com>
2020-01-10 19:11:59 -0800

    +helpers (#296)
    Add chai and dai

---
+++ src/tokens/chai.js
@@ -0,0 +1,23 @@
+const { truffleContract } = require("../helpers");
+const chaiJson = require("./chai.json");
+
+async function getToken(web3, tokenAddress) {
+  return await truffleContract.at(web3, chaiJson.abi, tokenAddress);
+}
+
+module.exports = {
+  mainnetAddress: "0x06af07097c9eeb7fd685c692751d5c66db49c215",
+  decimals: 18,
+  deploy: async (web3, from) => {
+    const result = await truffleContract.new(
+      web3,
+      chaiJson.abi,
+      `0x${chaiJson.bytecode.replace(/0x/, "")}`,
+      from,
+      []
+    );
+
+    return result;
+  },
+  getToken
+};
---
+++ src/tokens/dai.js
@@ -0,0 +1,39 @@
+const { truffleContract } = require("../helpers");
+const daiJson = require("./dai.json");
+const utils = require("../utils");
+
+async function getToken(web3, tokenAddress) {
+  return await truffleContract.at(web3, daiJson.abi, tokenAddress);
+}
+
+module.exports = {
+  mainnetAddress: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  decimals: 18,
+  /**
+   * @param useAntiOwner true to use a proxy contract allowing any account to call `mint`.
+   */
+  deploy: async (web3, from, useAntiOwner) => {
+    const result = await truffleContract.new(
+      web3,
+      daiJson.abi,
+      `0x${daiJson.bytecode.replace(/0x/, "")}`,
+      from,
+      daiJson.args[0]
+    );
+
+    if (useAntiOwner) {
+      const antiOwnerProxy = await utils.antiOwnerProxy.deploy(web3, from);
+      await result.rely(antiOwnerProxy.address, { from });
+      result.mint = async (to, amount, options) => {
+        const callData = web3.eth.abi.encodeFunctionCall(
+          daiJson.abi.find(e => e.name === "mint"),
+          [to, amount]
+        );
+        await antiOwnerProxy.proxyCall(result.address, callData, options);
+      };
+    }
+
+    return result;
+  },
+  getToken
+};
