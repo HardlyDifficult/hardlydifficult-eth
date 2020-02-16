68d3fa7a3e0f840639656b4f951f5b67cde5a16c
Hardly Difficult <hardlydiff@gmail.com>
2020-02-16 12:52:24 -0800

    Switch Stoppable to an "Admin" role (#325)
    * Switch Stoppable to an "Admin" role
    
    * Bump package version

---
+++ contracts/access/roles/AdminRole.sol
@@ -0,0 +1,56 @@
+pragma solidity ^0.5.0;
+
+import '@openzeppelin/contracts/access/Roles.sol';
+
+/**
+ * @title AdminRole
+ * @notice A copy of one of the OpenZeppelin roles with a more general purpose name.
+ * Original source: https://github.com/OpenZeppelin/openzeppelin-contracts/tree/master/contracts/access
+ */
+contract AdminRole
+{
+  using Roles for Roles.Role;
+
+  event AdminAdded(address indexed account);
+  event AdminRemoved(address indexed account);
+
+  Roles.Role private _admins;
+
+  constructor () internal
+  {
+    _addAdmin(msg.sender);
+  }
+
+  modifier onlyAdmin()
+  {
+    require(isAdmin(msg.sender), 'AdminRole: caller does not have the Admin role');
+    _;
+  }
+
+  function isAdmin(address account) public view returns (bool)
+  {
+    return _admins.has(account);
+  }
+
+  function addAdmin(address account) public onlyAdmin
+  {
+    _addAdmin(account);
+  }
+
+  function renounceAdmin() public
+  {
+    _removeAdmin(msg.sender);
+  }
+
+  function _addAdmin(address account) internal
+  {
+    _admins.add(account);
+    emit AdminAdded(account);
+  }
+
+  function _removeAdmin(address account) internal
+  {
+    _admins.remove(account);
+    emit AdminRemoved(account);
+  }
+}
