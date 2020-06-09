f83236320c14e82338b24713d0b25365f41ffd62
Hardly Difficult <hardlydiff@gmail.com>
2020-06-09 11:43:04 -0700

    Upgrade to Uniswap router 2 (#381)

--- src/protocols/uniswapV2.js
+++ src/protocols/uniswapV2.js
@@ -3,7 +3,7 @@ const uniswapJson = require("./uniswapV2.json");
 
 module.exports = {
   mainnetFactoryAddress: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
-  mainnetRouterAddress: "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a",
+  mainnetRouterAddress: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
   /**
    * Deploys the Uniswap factory and router.
    * @param feeSetter allows a fee to be enabled. May be address(0)
